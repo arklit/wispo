7ea3faa7ca8f68c515f517fbc77aefeb8a40bcd0
Misty <artemoutside@gmail.com>
2022-05-22 12:46:21 +0300

    768px

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,13 @@
 import './index.scss'
+
+const text = document.querySelector('.service__text')
+const mobile = window.matchMedia('(max-width: 1023px)')
+
+function mobileFn() {
+  if(mobile.matches) {
+    text.textContent = 'Ремонт планшетов в Челябинске. Бесплатно продиагностируем и быстро починим любую поломку'
+  } else {
+  text.textContent ="Ремонт планшетов в Челябинске. Бесплатно продиагностируем и быстро починим любую поломку. Гарантия 3 месяца."
+  }
+}
+mobileFn()
