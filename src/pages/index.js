7c27cfff7ac43b77f4caacf557e83cd4586592d3
Misty <artemoutside@gmail.com>
2022-05-23 13:56:48 +0300

    all sizes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,25 @@
 import './index.scss'
+const headerMobile= require('../images/headerMobile.png')
+const headerDesktop = require('../images/header.png')
 
 const text = document.querySelector('.service__text')
-const mobile = window.matchMedia('(max-width: 1023px)')
+const size1023 = window.matchMedia('(max-width: 1023px)')
+const size767 = window.matchMedia('(max-width: 767px)')
+const headerImg = document.querySelector('.header__img')
 
-function mobileFn() {
-  if(mobile.matches) {
+function size1024() {
+  if(size1023.matches) {
     text.textContent = 'Ремонт планшетов в Челябинске. Бесплатно продиагностируем и быстро починим любую поломку'
   } else {
   text.textContent ="Ремонт планшетов в Челябинске. Бесплатно продиагностируем и быстро починим любую поломку. Гарантия 3 месяца."
   }
 }
-mobileFn()
+function size768px() {
+  if(size767.matches) {
+    headerImg.src = headerMobile
+  } else {
+    headerImg.src = headerDesktop
+  }
+}
+size768px()
+size1024()
