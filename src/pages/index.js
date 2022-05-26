1e82fcbaad80bfafe6f0e0140029da98bcf4f2b4
Misty <artemoutside@gmail.com>
2022-05-26 14:07:04 +0300

    over

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,9 @@ const text = document.querySelector('.service__text')
 const size1023 = window.matchMedia('(max-width: 1023px)')
 const size767 = window.matchMedia('(max-width: 767px)')
 const headerImg = document.querySelector('.header__img')
-
+const menuButton = document.querySelector('.header__menu')
+const menu = document.querySelector('.menu')
+const menuCloseButton = document.querySelector('.menu__close')
 function size1024() {
   if(size1023.matches) {
     text.textContent = 'Ремонт планшетов в Челябинске. Бесплатно продиагностируем и быстро починим любую поломку'
@@ -21,5 +23,13 @@ function size768px() {
     headerImg.src = headerDesktop
   }
 }
+function menuHandler() {
+  menu.classList.add('menu_opened')
+}
+function menuCloseHandler() {
+  menu.classList.remove('menu_opened')
+}
+menuCloseButton.addEventListener('click', menuCloseHandler)
+menuButton.addEventListener('click', menuHandler)
 size768px()
 size1024()
